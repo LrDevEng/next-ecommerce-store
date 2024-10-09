5932b353ba4af574fe74465a700824266358aa0f
LrDevEng <lr.deveng@gmail.com>
2024-10-09 16:06:38 +0200

    Add icons to nav bar when in small mode

--- app/01-generic-components/NavBar.js
+++ app/01-generic-components/NavBar.js
@@ -26,9 +26,38 @@ export default async function NavBar() {
       <Link className={styles.eightBit} href="/">
         Custom ARCADE
       </Link>
-      <Link href="/about">About</Link>
+      <Link href="/about">
+        <div className={styles.linkBig}>About</div>
+        <div className={styles.linkSmall}>
+          <Image
+            src="/icons/icon-info.svg"
+            alt="About"
+            width={48}
+            height={48}
+          />
+        </div>
+      </Link>
+      <Link href="/blog">
+        <div className={styles.linkBig}>Blog</div>
+        <div className={styles.linkSmall}>
+          <Image
+            src="/icons/icon-newspaper.svg"
+            alt="About"
+            width={48}
+            height={48}
+          />
+        </div>
+      </Link>
       <Link href="/shop" data-test-id="products-link">
-        Shop
+        <div className={styles.linkBig}>Shop</div>
+        <div className={styles.linkSmall}>
+          <Image
+            src="/icons/icon-shopping-bag.svg"
+            alt="Shop"
+            width={48}
+            height={48}
+          />
+        </div>
       </Link>
       <Link className={styles.cartLink} href="/cart" data-test-id="cart-link">
         <Image src="/icons/icon-cart.svg" alt="Cart" width={48} height={48} />
