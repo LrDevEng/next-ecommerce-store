d904e054d92aa7e8adc6c01682bc43d7face5e4a
LrDevEng <lr.deveng@gmail.com>
2024-10-11 13:05:21 +0200

    Update landing page

--- app/(main)/components/ShopNowCard.js
+++ app/(main)/components/ShopNowCard.js
@@ -1,12 +1,26 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import { useFilterType } from '../../stores/useFilterType';
 import styles from './ShopNowCard.module.css';
 
-export default function ShopNowCard({ heading, className }) {
+export default function ShopNowCard({ heading, productType, className }) {
+  const router = useRouter();
+
+  // Get filter state
+  const updateFilterType = useFilterType((state) => state.update);
+
   return (
     <div className={`${styles.container} ${className}`}>
       <h3>{heading}</h3>
-      <button>Shop Now</button>
+      <button
+        onClick={() => {
+          updateFilterType(productType);
+          router.push('/shop');
+        }}
+      >
+        Shop Now
+      </button>
     </div>
   );
 }
--- app/(main)/page.js
+++ app/(main)/page.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import { productType } from '../util/constants';
 import ShopNowCard from './components/ShopNowCard';
 import styles from './page.module.css';
 
@@ -6,17 +8,36 @@ export default function HomePage() {
     <div className={styles.page}>
       <h2>WELCOME TO</h2>
       <h1>Custom ARCADE</h1>
-      <h3>Your trusted platform for building custom arcade machines.</h3>
+      <h3 className={styles.sloganPlatform}>
+        Your trusted platform for building custom arcade machines.
+      </h3>
       <div className={styles.sloganContainer}>
         <h2 className={styles.sloganStart}>Learn.</h2>
         <h2 className={styles.sloganMiddle}>Create.</h2>
         <h2 className={styles.sloganEnd}>Enjoy!</h2>
       </div>
       <h2>GET PARTS</h2>
-      <div>
-        <ShopNowCard className={styles.red} heading="Controllers" />
-        <ShopNowCard className={styles.blue} heading="Buttons" />
-        <ShopNowCard className={styles.yellow} heading="Joysticks" />
+      <div className={styles.shopNow}>
+        <ShopNowCard
+          className={styles.shopNowStart}
+          heading="Controllers"
+          productType={productType.microcontroller}
+        />
+        <ShopNowCard
+          className={styles.shopNowMiddle}
+          heading="Buttons"
+          productType={productType.button}
+        />
+        <ShopNowCard
+          className={styles.shopNowEnd}
+          heading="Joysticks"
+          productType={productType.joystick}
+        />
+      </div>
+      <div className={styles.blog}>
+        <h2>Featured Blog Post</h2>
+        <div className={styles.blogPost}>Area for featured blog post.</div>
+        <Link href="/blog">Read more.</Link>
       </div>
     </div>
   );
