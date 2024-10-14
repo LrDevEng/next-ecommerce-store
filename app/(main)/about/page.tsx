b0b46df40a487de8a0be027d1e5c5c2170d39fce
LrDevEng <lr.deveng@gmail.com>
2024-10-14 23:26:11 +0200

    Update about page

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from './page.module.css';
 
 export const metadata = {
@@ -10,21 +11,81 @@ export default function AboutPage() {
     <div className={styles.page}>
       <h1>About</h1>
       <div>
-        <h3>Credits</h3>
+        <br />
+        <h2>Credits</h2>
         <p>
-          This platform is an example project created by <span>LrDevEng.</span>
+          This website is a full stack example project of an e-commerce platform
+          <br />
+          created by software developer{' '}
+          <Link href="https://github.com/LrDevEng" target="_blank">
+            LrDevEng
+          </Link>
+          .
+        </p>
+        <p>
+          View the project on{' '}
+          <Link
+            href="https://github.com/LrDevEng/next-ecommerce-store"
+            target="_blank"
+          >
+            GitHub
+          </Link>
+          .
         </p>
       </div>
+      <br />
       <div>
-        <h3>Resources</h3>
-        <p>Background image designed by:</p>
-        <p>Social media icons designed by:</p>
-        <p>Navigation icons designed by:</p>
+        <h2>Disclaimer</h2>
+        <p>
+          This is not a real shop. The presented products cannot be purchased on
+          this website. Sole purpose of this project is to showcase the
+          development and setup of a full stack web application.
+        </p>
       </div>
+      <br />
       <div>
-        <h3>Product location</h3>
-        <p>The product images were taken from the following shops:</p>
-        <p>Give them a visit if you would like to pruchase some products.</p>
+        <h2>Resources</h2>
+        <p>
+          Background image designed by:{' '}
+          <Link href="http://www.freepik.com/" target="_blank">
+            Freepik
+          </Link>
+        </p>
+        <p>Social media icons designed by: </p>
+        <p>
+          Navigation icons designed by:{' '}
+          <Link href="https://fonts.google.com/icons" target="_blank">
+            Google
+          </Link>
+        </p>
+      </div>
+      <br />
+      <div>
+        <h2>Product Samples</h2>
+        <p>
+          The product images and prices were copied from the following shops
+          (October 2024):
+        </p>
+        <br />
+        <ul>
+          <li>
+            <Link href="https://berrybase.at/" target="_blank">
+              https://berrybase.at/
+            </Link>
+          </li>
+          <li>
+            <Link href="https://paradisearcadeshop.com/" target="_blank">
+              https://paradisearcadeshop.com/
+            </Link>
+          </li>
+          <li>
+            <Link href="https://botland.de/" target="_blank">
+              https://botland.de/
+            </Link>
+          </li>
+        </ul>
+        <br />
+        <p>Give them a visit if you would like to pruchase the products.</p>
       </div>
     </div>
   );
